a41aef335e11e42331a6f688b200b117ef0d342d
damelalbertos <damelalbertos@icloud.com>
2019-12-04 12:47:15 -0500

    Database is all up to date 2.0

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     dob = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -49,11 +51,12 @@ class Event(db.Model):
 
 
 class Friends(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    friend_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Friends {}>'.format(self.user_id)
+        return '<Friends {}>'.format(self.id)
 
 
 class Notification(db.Model):
