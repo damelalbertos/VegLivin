412a1f8611485ff2e41ab6bb32647652b2aabac1
aelliott <aelliott@ithaca.edu>
2019-12-09 12:53:53 -0500

    adding more style

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+import bp as bp
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
